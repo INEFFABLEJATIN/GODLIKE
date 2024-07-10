bbea2875942497acc0fb7843ede4fdafe0b5669d
INEFFABLEJATIN <168161391+INEFFABLEJATIN@users.noreply.github.com>
2024-07-10 09:43:48 +0530

    nestedloop.cpp

--- nestedloop.cpp
+++ nestedloop.cpp
@@ -13,3 +13,17 @@ int main ()
     cout << i << endl;
     }
 }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i=1; i<=4; i++)
+{
+    for (int j=1; j<=4; j++)
+    cout << "#" << i <<endl;
+}
+    cout<<endl;
+}
+
+
