5161620c2c7862ccb9ac14991a5d705b42a0de7d
INEFFABLEJATIN <168161391+INEFFABLEJATIN@users.noreply.github.com>
2024-07-20 21:06:00 +0530

    nestedloop.cpp

--- nestedloop.cpp
+++ nestedloop.cpp
@@ -37,3 +37,85 @@ int main()
 if remove the endl after i then #1#1#1#1 
 if remove i also then ####  like this in 4 consecutive line */
 
+
+/* inverted patterns */
+#include <iostream>
+using namespace std;
+int main ()
+{    //outer loop
+
+    for   ( int  i=1 ; i<=5; i++)
+    
+    //inner loop
+    {
+    for ( int j=1; j<=6-i; ++j)            // or define n also use it in outer loop; for (int j=1; j<=(n-i+1); j++
+    {
+        //work 
+        cout<<"#";
+    }
+      cout << endl;
+    } 
+}
+
+/* 11111
+   2222
+   333
+   44
+   5 */
+#include <iostream>
+using namespace std;
+int main ()
+{    //outer loop
+
+    for   ( int  i=1 ; i<=5; i++)
+    
+    //inner loop
+    {
+    for ( int j=1; j<=6-i; ++j)
+    {
+        //work 
+        cout<<i;
+    }
+      cout << endl;
+    } 
+}
+
+
+#include <iostream>
+using namespace std;
+int main ()                                             /* 11111 
+                                                           2222*/
+{    //outer loop                                          333
+                                                          //44
+    for   ( int  i=1 ; i<=5; i++)                         //5 
+    
+    //inner loop
+    {
+    for ( int j=1; j<=i; ++j)
+    {
+        //work 
+        cout<<i;
+    }
+      cout << endl;
+    } 
+}
+
+this one will print   1 12 123 1234 12345   
+
+#include <iostream>
+using namespace std;
+int main ()
+{    //outer loop
+
+    for   ( int  i=1 ; i<=5; i++)
+    
+    //inner loop
+    {
+    for ( int j=1; j<=i; ++j)
+    {
+        //work 
+        cout<<5;
+    }
+      cout << endl;
+    } 
+}
